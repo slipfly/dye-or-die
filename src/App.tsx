fe5096581493936be3d36ecd217a8fdb70b9817d
dina k <slipfly@gmail.com>
2025-06-16 18:46:00 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles/global.css';
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
 
 const App: React.FC = () => {
     return (
         <>
         <Header></Header>
+        <Footer></Footer>
         </>
     );
 };
