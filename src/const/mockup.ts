82304bfabb9d63e15c598baf987249f56c89228b
dina k <slipfly@gmail.com>
2025-06-17 17:48:05 +0300

    Create mockup.ts

---
+++ src/const/mockup.ts
@@ -0,0 +1,22 @@
+import { Material } from "../types/types";
+
+export const mockMaterials: Material[] = [
+    {
+        name: 'Порошок эстель',
+        cost: 650,
+        amount: 500,
+        cpu: 1.3
+    },
+    {
+        name: 'Перчатки',
+        cost: 340,
+        amount: 100,
+        cpu: 3.4
+    },
+    {
+        name: 'Оксид 3%',
+        cost: 1300,
+        amount: 1000,
+        cpu: 1.3
+    }
+];
