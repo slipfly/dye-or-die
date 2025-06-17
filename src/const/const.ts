5d4328266a93e15e581c2bedb3e28b04261fec44
dina k <slipfly@gmail.com>
2025-06-17 17:48:18 +0300

    Create const.ts

---
+++ src/const/const.ts
@@ -0,0 +1,12 @@
+export const myRoutes = {
+    default: '/',
+    materials: '/materials',
+    new: '/new',
+    portfolio: '/portfolio'
+} as const;
+
+export const MainNavigationButtons = {
+    new: 'New',
+    materials: 'Materials',
+    portfolio: 'Portfolio'
+} as const;
