6200d91f1c711b026da3004b7f75be26eaecb1a8
dina k <slipfly@gmail.com>
2025-06-17 17:45:44 +0300

    added Main page component

---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainNavigation from '../MainNavigation/MainNavigation';
+
+const MainPage: React.FC = () => {
+    return (
+        <main className="main">
+            <MainNavigation />
+        </main>
+    );
+};
+
+export default MainPage;
