159fd1f895db33477cd62c0c35ab30c5f512091a
dina k <slipfly@gmail.com>
2025-06-19 20:20:37 +0300

    divs -> btns

--- src/components/ControlButtons/ControlButtons.tsx
+++ src/components/ControlButtons/ControlButtons.tsx
@@ -1,31 +1,37 @@
 import React from 'react';
 import * as styles from './ControlButtons.module.css'
 
+interface  ButtonParameters {
+    name: string;
+    type: "submit" | "reset" | "button" | undefined;
+}
+
 interface ControlButtonsProps {
-    buttonNames: string[];
+    btnParams: readonly ButtonParameters[];
     onClickMap?: {
         [key: string]: () => void;
     };
 }
 
 const getControlButtons = (
-        buttonNames: string[],
-        onClickMap: { [key: string]: () => void } = {}
+    btnParams: readonly ButtonParameters[],
+    onClickMap: { [key: string]: () => void } = {}
     ) => {
-    return buttonNames.map((btn) => (
-        <div
-            key={btn}
-            className={`${styles["control-buttons__button"]} control-buttons__button--${btn.toLowerCase()}`}
-            onClick={onClickMap[btn]}>
-            {btn}
-        </div>
+    return btnParams.map((btn) => (
+        <button
+            key={btn.name}
+            type={btn.type}
+            className={`${styles["control-buttons__button"]} control-buttons__button--${btn.name.toLowerCase()}`}
+            onClick={onClickMap[btn.name]}>
+            {btn.name}
+        </button>
     ));
 };
 
-const ControlButtons: React.FC<ControlButtonsProps> = ({ buttonNames, onClickMap }) => {
+const ControlButtons: React.FC<ControlButtonsProps> = ({ btnParams, onClickMap }) => {
     return (
         <section className={styles["control-buttons"]}>
-            {getControlButtons(buttonNames, onClickMap || {})}
+            {getControlButtons(btnParams, onClickMap || {})}
         </section>
     );
 };
